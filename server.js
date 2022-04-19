5b1c751417fad8e4dbe373a9a293de10dc84fea0
Anthony Malary <amalary@bu.edu>
2022-04-19 10:06:41 -0700

    Set up basic structure of Express App

---
+++ server.js
@@ -0,0 +1,27 @@
+// Require modules 
+
+const express = require('express'); 
+
+// Create the express app 
+
+const app = express(); 
+
+
+// Configure the app (app.set)
+
+
+
+// Mount middleware (app.use)
+
+
+
+// Mount routes
+app.get('/', function(req,res){
+    res.send('<h1>Hello World</h1>'); 
+})
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function(){
+    console.log('Listening on port 3000')
+})
